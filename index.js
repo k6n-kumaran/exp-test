ec2ff646531e738e1f6c4f90572006393cb4e794
gunakrn <gunakumaran01.gmail.com>
2024-08-04 09:15:45 +0530

    adding files to git

---
+++ index.js
@@ -0,0 +1,59 @@
+import express from 'express'
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+let teaData = [];
+let nextId = 1;
+
+app.get("/teas"  ,(req,res) => {
+    res.status(200).send(teaData)
+}); 
+
+app.post("/teas", (req,res) => {
+     const {name,price } = req.body;
+     let newItem = {id : nextId++ , name, price}
+     teaData.push(newItem)
+     res.status(201).send(newItem)
+})
+
+app.get("/teas/:id" , (req,res) => {
+ const tea = teaData.find(t => t.id  === parseInt(req.params.id))
+ if(!tea) {
+    res.status(404)
+ } 
+    res.status(200).send(tea)
+ 
+})
+
+app.put("/teas/:id" ,  (req,res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+
+    if(!tea) {
+        res.status(404).send("Tea not found")
+    }
+
+    const {name,price} = req.body;
+    tea.name = name;
+    tea.price = price;
+
+    res.status(201).send(tea)
+})
+
+
+app.delete("/teas/:id", (req,res) => {
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id));
+
+    if(index === -1) {
+        return res.status(404).send("NOT FOUND")
+
+    }
+    teaData.splice(index,1)
+    return res.status(201).send("DELETED")
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on localhost ${port}`);
+})
